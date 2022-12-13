3a58a6c210441899ec0ecea06c4081dc701896fe
chaoyuepan <chaoyuepan@baidu.com>
2022-12-13 18:48:25 +0800

    add tcp code

---
+++ cmd/tcp/client/tcpclient.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/smallnest/network_benchmark/stat"
+	"github.com/smallnest/network_benchmark/tcp"
+)
+
+var (
+	localAddr   = flag.String("laddr", "", "local address")
+	localPorts  = flag.String("lports", "20000,20016", "local ports")
+	serverPorts = flag.String("sports", "20000,20016", "server ports")
+	serverAddr  = flag.String("saddr", "", "server address")
+	pkgSize     = flag.Int("pkgsize", 64, "package size")
+	rate        = flag.Int("rate", 10000, "rate")
+)
+
+func main() {
+	flag.Parse()
+
+	localPorts := strings.Split(*localPorts, ",")
+	serverPorts := strings.Split(*serverPorts, ",")
+
+	lportMin, _ := strconv.Atoi(localPorts[0])
+	sportMin, _ := strconv.Atoi(serverPorts[0])
+	lportMax, _ := strconv.Atoi(localPorts[1])
+	// sportMax, _ := strconv.Atoi(serverPorts[1])
+	count := lportMax - lportMin
+
+	rate := *rate / count
+
+	var aggrStat = stat.NewAggrStat()
+
+	var clients []*tcp.Client
+	for i := 0; i < count; i++ {
+		lport := lportMin + i
+		sport := sportMin + i
+
+		client := tcp.NewClient(*localAddr, lport, *serverAddr, sport, *pkgSize, rate, aggrStat)
+		clients = append(clients, client)
+
+		go client.Run()
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+}
---
+++ tcp/client.go
@@ -0,0 +1,95 @@
+package tcp
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/smallnest/network_benchmark/stat"
+	"go.uber.org/ratelimit"
+)
+
+type Client struct {
+	localAddr  string
+	localPort  int
+	serverAddr string
+	serverPort int
+	pktSize    int
+	rate       int
+
+	conn  *net.TCPConn
+	stats *stat.Stats
+}
+
+func NewClient(localAddr string, localPort int, serverAddr string, serverPort int, pktSize int, rate int, aggrStat *stat.AggrStat) *Client {
+	return &Client{
+		localAddr:  localAddr,
+		localPort:  localPort,
+		serverAddr: serverAddr,
+		serverPort: serverPort,
+		pktSize:    pktSize,
+		rate:       rate,
+		stats:      stat.NewStats(fmt.Sprintf("%s:%d->%s:%d", localAddr, localPort, serverAddr, serverPort), aggrStat),
+	}
+}
+
+func (c *Client) Run() {
+	defer func() {
+		time.Sleep(5 * time.Second)
+		c.stats.Close()
+	}()
+
+	srcAddr := &net.TCPAddr{IP: net.ParseIP(c.localAddr), Port: c.localPort}
+	dstAddr := &net.TCPAddr{IP: net.ParseIP(c.serverAddr), Port: c.serverPort}
+
+	conn, err := net.DialTCP("tcp", srcAddr, dstAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.conn = conn
+	go c.read()
+
+	data := bytes.Repeat([]byte("a"), c.pktSize)
+
+	rateLimter := ratelimit.New(c.rate, ratelimit.Per(time.Second))
+
+	for seq := uint64(0); ; seq++ {
+		rateLimter.Take()
+		ts := time.Now().UnixNano()
+		binary.LittleEndian.PutUint64(data[:8], seq)
+		binary.LittleEndian.PutUint64(data[8:16], uint64(ts))
+		c.stats.AddSent(seq, ts)
+		_, err = conn.Write(data)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (c *Client) read() {
+	data := make([]byte, c.pktSize)
+
+	for {
+		n, err := c.conn.Read(data)
+		if err != nil {
+			return
+		}
+		if n < c.pktSize {
+			continue
+		}
+
+		seq := binary.LittleEndian.Uint64(data[:8])
+		//ts := binary.LittleEndian.Uint64(data[8:16])
+
+		c.stats.AddRecv(seq, time.Now().UnixNano())
+	}
+}
+
+func (c *Client) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
