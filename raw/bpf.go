9a8e1c5d485bb68ea54fb864a4c6c74f657a8fe4
chaoyuepan <chaoyuepan@baidu.com>
2023-05-30 14:39:53 +0800

    add raw

--- raw/bpf.go
+++ raw/bpf.go
@@ -12,10 +12,10 @@ func createFilter(minPort, maxPort int, tos int) []bpf.Instruction {
 	golog.Infof("set filter for %d ~ %d, tos: %d", minPort, maxPort, tos)
 	filter := Filter{
 		bpf.LoadIndirect{Off: 9, Size: 1},
-		bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(17), SkipFalse: 6}, //UDP proto = 17
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(17), SkipFalse: 4}, //UDP proto = 17
 
-		bpf.LoadIndirect{Off: 1, Size: 1},
-		bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(tos), SkipFalse: 4}, //tos
+		// bpf.LoadIndirect{Off: 1, Size: 1},
+		// bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(tos), SkipFalse: 4}, //tos
 
 		bpf.LoadAbsolute{Off: 22, Size: 2}, // load the dest port
 		bpf.JumpIf{Cond: bpf.JumpGreaterOrEqual, Val: uint32(minPort), SkipFalse: 2},
